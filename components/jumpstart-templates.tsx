7b51d053f7bcccb39a574aaef921b7b71041c45f
bimsara132 <bimsarabandara02@gmail.com>
2026-02-13 00:56:08 +0530

    Update

--- components/jumpstart-templates.tsx
+++ components/jumpstart-templates.tsx
@@ -54,7 +54,7 @@ export function JumpstartTemplates({
   return (
     <>
       <div className="w-full max-w-6xl mx-auto px-2 sm:px-4 min-w-0">
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 sm:gap-3">
+        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 sm:gap-3">
           {templates.map((template) => (
             <div
               key={template.id}
